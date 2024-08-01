8c1d2a95c09d6a98dea40653145417c56910fdcf
yinsel <yinsel@163.com>
2024-08-01 11:55:08 +0800

    修改minSdk为26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.yinsel.checkproxy"
-        minSdk = 29
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
